748be9f42ba19f4315a8e9507048d107f9c532d8
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-04-03 11:19:41 -0300

    init layout sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./global.css"
-
+import twitterLogo from "./assets/logo.twitter.svg"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>Hello world</div>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="logo" />
+
+        <nav></nav>
+
+        <button>Tweet</button>
+      </aside>
+      <div className="content">
+        CONTENT
+      </div>
+
+    </div>
   </React.StrictMode>,
 )
