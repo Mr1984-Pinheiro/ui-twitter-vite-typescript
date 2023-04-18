1449ea386fbdc60a902aec07c3c7bea56e13427d
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-04-17 21:30:42 -0300

    layout default

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "./global.css"
-import { Sidebar } from './components/Sidebar';
 import { router } from './components/routes';
 import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div className="layout">
-     <Sidebar />     
-      <div className="content">
-      <RouterProvider router={router} />             
-      </div>
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
