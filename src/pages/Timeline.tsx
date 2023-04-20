391992022f525666e1c79e738f00f0d69e604552
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2023-04-19 22:20:01 -0300

    estados da aplicação

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -2,21 +2,38 @@ import { Tweet } from '../components/Tweet';
 import { Header } from '../components/Header';
 import { Separator } from '../components/Separator';
 import "./Timeline.css"
+import { FormEvent, useState } from 'react';
 
-const tweets = [
+
+
+let newTweet = ''
+
+export function Timeline () {
+  const [tweets, setTweets] = useState([
     'Meu primeiro tweet',
     'teste',
     'Deu certo tweetar!'
-  ]
+  ]);
 
-export function Timeline () {
+  function createNewTweet (event: FormEvent) {
+    event.preventDefault();
+
+    console.log(newTweet);
+  }
+
     return (
         <main className="timeline">          
           <Header title="Home" />
-          <form className="new-tweet-form">
+          <form onSubmit={createNewTweet} className="new-tweet-form">
             <label htmlFor="tweet">
               <img src="https://github.com/Mr1984-Pinheiro.png" alt="Carlos Pinheiro" />
-              <textarea id='tweet' placeholder='O que é que há, velhinho?' />
+              <textarea
+               id='tweet' 
+               placeholder='O que é que há, velhinho?' 
+               onChange={(event) => {
+                  newTweet = event.target.value;
+               }}
+               />
             </label>
             <button type='submit'>Tweet</button>
           </form>
